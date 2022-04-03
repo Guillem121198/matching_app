3f3e481eaee84209d6c95f3402b9141cd109a468
Guillem Angelini <43513397+Guillem121198@users.noreply.github.com>
2022-04-03 21:09:24 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,10 +25,10 @@ players_22 = pd.read_csv(url, sep=',')
 
 st.write(players_22)
 
-url_3 = 'https://raw.githubusercontent.com/Guillem121198/matching_app/main/bikes.csv'
-bikes = pd.read_csv(url_3, sep=',')
+url_3 = 'https://raw.githubusercontent.com/Guillem121198/matching_app/main/chicago_crimes.csv'
+crimes = pd.read_csv(url_3, sep=',')
 
-st.write(bikes)
+st.write(crimes)
 
 url_2 = 'https://raw.githubusercontent.com/Guillem121198/matching_app/main/yoyo_match_results.xlsx'
 match_table = pd.read_excel(url_2)
