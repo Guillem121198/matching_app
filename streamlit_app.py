d482f6e3ade88baf6bed6a075923f5a769316c6c
Guillem Angelini <43513397+Guillem121198@users.noreply.github.com>
2022-04-03 20:09:07 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,12 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+
+# Pandas options
 pd.options.mode.chained_assignment = None
 pd.set_option("display.max_columns", None)
 
-
+# Streamlit
 st.title("Yoyo matchs app")
 
 input_name = st.text_input('Ton compte insta avec le @', '@yoyo_bdt')
@@ -17,8 +19,9 @@ st.write(pd.DataFrame({
     'second column': [10, 20, 30, 40]
 }))
 
-
+# Python script
 url = 'https://raw.githubusercontent.com/Guillem121198/machine_learning/main/players_22.csv'
 players_22 = pd.read_csv(url, sep=',')
 
 st.write(players_22)
+
