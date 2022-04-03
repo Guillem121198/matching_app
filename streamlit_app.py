ab5e6b11f4e1185caeeb4af01e0e736b56dc3c71
Guillem Angelini <43513397+Guillem121198@users.noreply.github.com>
2022-04-03 12:44:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import pandas as pd
 
+st.title("Yoyo matchs app")
+
+input_name = st.text_input('Ton compte insta avec le @', '@yoyo_bdt')
+
 st.write("Here's our first attempt at using data to create a table:")
 st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
